2973bdcb2a0be5974f7c844d538bf5f86611fbb8
nijz <nijenbhut@gmail.com>
2025-05-25 12:43:25 +0530

    feat: add CompanionForm component with form validation and UI enhancements
    - Introduced CompanionForm component for user input on companion creation.
    - Integrated Zod for schema validation and react-hook-form for form handling.
    - Added UI components for form fields including Input, Select, and Textarea.
    - Implemented Radix UI components for better accessibility and styling.
    - Updated package.json to include necessary dependencies: @hookform/resolvers, @radix-ui/react-label, @radix-ui/react-select, react-hook-form, and zod.

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,11 +1,15 @@
+import CompanionForm from '@/components/companion/CompanionForm'
 import React from 'react'
 
 const NewCompanion = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center mx-auto'>
+            <article className='w-full flex flex-col gap-4'>
+                <h1>Companion Builder</h1>
+                <CompanionForm/>
+            </article>
+        </main>
+    )
 }
 
 export default NewCompanion
