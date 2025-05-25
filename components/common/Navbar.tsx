6cc0399d93053bacdd247ae2f38d19b5afa0ae36
nijz <nijenbhut@gmail.com>
2025-05-25 17:30:41 +0530

    feat: integrate Clerk authentication and Supabase client; update Navbar and layout components

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import NavItems from '../navbar/NavItems'
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const Navbar = () => {
     return (
@@ -18,6 +24,16 @@ const Navbar = () => {
             </Link>
             <div className='flex items-center gap-8'>
                 <NavItems/>
+                <SignedOut>
+                    <SignInButton>
+                        <button className='btn-signin'>
+                            Sign In
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
